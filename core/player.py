4a591f678af240d293e0aa61d75d423a83c95f62
ivan-resetnikov-a <ivan.resetnikov.alpha@gmail.com>
2023-02-28 09:31:31 +0200

    Basic structure for game

---
+++ core/player.py
@@ -0,0 +1,6 @@
+import pygame as pg
+
+
+
+class Player :
+	pass
---
+++ main.py
@@ -0,0 +1,59 @@
+import pygame as pg
+pg.mixer.init()
+pg.font.init()
+pg.init()
+
+font = pg.font.Font('assets/fonts/pixeloid.ttf', 9)
+
+
+
+class TF1 :
+	def __init__ (self) :
+		# config
+		self.windowSize = (1920, 1080)
+		self.renderSize = (480, 270)
+		self.title = 'Team Fortress 1'
+
+		self.fps = 60
+
+		# initialise window
+		self.window = pg.display.set_mode(self.windowSize, pg.FULLSCREEN)
+		self.frame  = pg.Surface(self.renderSize)
+		self.clock  = pg.time.Clock()
+
+
+	def run (self) :
+		self.runnning = True
+
+		while self.runnning :
+			for event in pg.event.get() :
+				if event.type == pg.QUIT :
+					self.runnning = False
+
+			self.update()
+			self.render()
+
+		pg.mixer.quit()
+		pg.font.quit()
+		pg.quit()
+
+
+	def update (self) :
+		pass
+
+
+	def render (self) :
+		# update screen
+		self.frame.fill((0, 0, 0))
+
+		# render
+		self.frame.blit(font.render(str(round(self.clock.get_fps())), 0, (255, 255, 255)), (0, 0))
+
+		# apply buffer
+		self.window.blit(pg.transform.scale(self.frame, self.windowSize), (0, 0))
+		self.clock.tick(self.fps)
+		pg.display.flip()
+
+
+
+TF1().run()
